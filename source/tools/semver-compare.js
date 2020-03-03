d93a8c10015e212d878f56be5d5998a5aaad7625
Alan-Liang <a@keeer.net>
2020-03-03 10:20:09 +0800

    fix semver comparison

--- source/tools/semver-compare.js
+++ source/tools/semver-compare.js
@@ -4,8 +4,8 @@
 // Inlining this function because some users reported issues with
 // importing from `semver-compare` in a browser with ES6 "native" modules.
 export default function cmp (a, b) {
-    var pa = a.split('.');
-    var pb = b.split('.');
+    var pa = a.split('-')[0].split('.');
+    var pb = b.split('-')[0].split('.');
     for (var i = 0; i < 3; i++) {
         var na = Number(pa[i]);
         var nb = Number(pb[i]);
